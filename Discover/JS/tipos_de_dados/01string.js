ae08cf369b528a50eacd104bbbe1fad9e2312178
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-03-08 15:18:55 -0300

    add string/numbers

---
+++ Discover/JS/tipos_de_dados/01string.js
@@ -0,0 +1,11 @@
+/*
+Cadeia de caracteres (palavras)
+
+" " 
+' ' 
+` ` 
+*/
+
+console.log(" teste com aspas duplas 'teste' ")
+console.log(' teste com aspas simples "teste" ')
+console.log(` teste com crases ("" '') ${1+1}`)
