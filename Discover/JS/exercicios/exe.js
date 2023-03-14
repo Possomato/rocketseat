418a701978b6e42b84d872ac70f2c0a2f8437e7a
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-03-14 10:10:36 -0300

    add exe.js

---
+++ Discover/JS/exercicios/exe.js
@@ -0,0 +1,79 @@
+//  1. Declare uma variável de nome weight
+
+// var weight
+
+
+//  2. Que tipo de dado é a variável acima?
+
+// console.log(typeof weight)
+
+/*
+    3. Declare uma variável e atribua valores para um dos dados:
+        * name: String
+        * age: Number (integer)
+        * stars: Number (float)
+        *  isSubscribed: boolean
+*/
+
+
+// let name = 'gabriel'
+// let age = 20
+// let stars = 7.2
+// let isSubscribed = true
+
+/*
+    4. A varoriável student abaixo é de que tipo de dado?
+
+    4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+    4.2 Mostre no console a seguinte mensagem:
+
+        <name> de idade <age> pesa <weight> pelos valores de cada propriedade do objeto
+*/
+
+// let student = {
+//     name: 'gabriel',
+//     age: 20,
+//     weight: 74.2,
+//     inSubscribed: true,
+// }
+
+// console.log(typeof student)
+
+// console.log(`${student.name} de idade ${student.age} pesa ${student.weight}kg pelos valores de cada propriedade do objeto`)
+
+/*
+    5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor
+*/
+
+// let students = []
+
+/*
+    6. Reatribua valor para a variável acima, colocando dentro dela o objeto student da questão 4
+*/
+
+// let students = [
+//     student
+// ]
+
+/*
+    7. Coloque no console o valor da posição zero do Arra acima
+*/
+
+// console.log(students[0])
+
+/*
+    8. Cria um novo student e coloque na posição 1 do Array students
+*/
+
+// const student2 = {
+//     name: 'João',
+//     age: 18,
+//     weight: 64.9,
+//     inSubscribed: true,
+// }
+
+// let students = [
+//     student,
+//     student2
+// ]
